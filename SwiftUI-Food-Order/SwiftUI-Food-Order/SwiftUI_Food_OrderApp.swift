19e285202fc65a8f00b4f18fb7455683753e432d
Ewald <57798772+EforestHD@users.noreply.github.com>
2022-04-08 13:07:20 +0200

    init

---
+++ SwiftUI-Food-Order/SwiftUI-Food-Order/SwiftUI_Food_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Food_OrderApp.swift
+//  SwiftUI-Food-Order
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Food_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
